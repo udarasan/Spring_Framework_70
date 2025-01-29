40408a4096ba11fe230ce8dcc4410b1353442109
udarasan <udarassanjeewa@gmail.com>
2025-01-29 09:41:09 +0530

    full mode and light mode project created

---
+++ 04_FullMode_Vs_LightMode/src/main/java/org/example/AppInitializer.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.example.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+
+
+        context.registerShutdownHook();
+
+
+    }
+}
