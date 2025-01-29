1ec016917d4ae5e1fb3145178191335272ae23ad
udarasan <udarassanjeewa@gmail.com>
2025-01-29 10:51:33 +0530

    configuration module created

---
+++ 05_Configuration/src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,12 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@ComponentScan(basePackages = "org.example.bean")
+public class AppConfig {
+
+}
