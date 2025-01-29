ca707bd6c3c27118cb33333194beb5128c30f40f
udarasan <udarassanjeewa@gmail.com>
2025-01-29 11:57:38 +0530

    instance property value injection

--- 06_Runtime_Value_Injection/src/main/java/org/example/bean/SpringBean.java
+++ 06_Runtime_Value_Injection/src/main/java/org/example/bean/SpringBean.java
@@ -1,24 +1,21 @@
 package org.example.bean;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean{
-    //public SpringBean(){}
-    @Autowired(required=false)
-    public SpringBean(@Value("udara-san") String name,
-                      @Value("6") String names) {
-        System.out.println("Spring Bean Object Created");
+public class SpringBean implements InitializingBean {
+    @Value("udara-san")
+    private String name;
+
+    public SpringBean(){
         System.out.println(name);
-        System.out.println(names);
-    }
-    @Autowired(required=false)
-    public SpringBean(@Value("udara-san") String name, @Value("3") int id) {
-        System.out.println("Spring Bean Object Created");
-        System.out.println(name);
-        System.out.println(id);
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(name);
+    }
 }
