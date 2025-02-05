09f8d8f4f32296daa30f83a2ea6b7aa1cc2750b5
udarasan <udarassanjeewa@gmail.com>
2025-02-05 14:03:03 +0530

    content update

--- 09_RestController/src/main/java/org/example/controller/FormDataController.java
+++ 09_RestController/src/main/java/org/example/controller/FormDataController.java
@@ -1,4 +1,14 @@
 package org.example.controller;
 
+import org.example.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("form")
 public class FormDataController {
+    @PostMapping
+    public String test1(@ModelAttribute CustomerDTO customerDTO) {
+        System.out.println(customerDTO.getFirstName());
+        return customerDTO.toString();
+    }
 }
