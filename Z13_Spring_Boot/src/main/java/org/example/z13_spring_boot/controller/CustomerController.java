7557a81dc0d415728f3c8aa2b217c4ecfa3fbce9
udarasan <udarassanjeewa@gmail.com>
2025-02-20 10:04:52 +0530

    update customer

--- Z13_Spring_Boot/src/main/java/org/example/z13_spring_boot/controller/CustomerController.java
+++ Z13_Spring_Boot/src/main/java/org/example/z13_spring_boot/controller/CustomerController.java
@@ -5,6 +5,8 @@ import org.example.z13_spring_boot.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
@@ -14,7 +16,21 @@ public class CustomerController {
 
     @PostMapping(path = "save")
     public Boolean getCustomer(@RequestBody CustomerDTO customerDTO) {
-        boolean res= customerService.save(customerDTO);
+        boolean res= customerService.saveCustomer(customerDTO);
         return res;
     }
+    @GetMapping(path ="getAll")
+    public List<CustomerDTO> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+    @PutMapping(path = "update")
+    public Boolean updateCustomer(@RequestBody CustomerDTO customerDTO) {
+        boolean res= customerService.updateCustomer(customerDTO);
+        return res;
+    }
+    @DeleteMapping(path = "delete/{id}")
+    public Boolean deleteCustomer(@PathVariable("id") int id) {
+        return customerService.deleteCustomer(id);
+    }
+
 }
--- Z13_Spring_Boot/src/main/java/org/example/z13_spring_boot/service/CustomerService.java
+++ Z13_Spring_Boot/src/main/java/org/example/z13_spring_boot/service/CustomerService.java
@@ -6,17 +6,46 @@ import org.example.z13_spring_boot.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CustomerService  {
     @Autowired
     private CustomerRepo customerRepo;
 
-    public boolean save(CustomerDTO customerDTO) {
+    public boolean saveCustomer(CustomerDTO customerDTO) {
         Customer customer = new Customer(
                 customerDTO.getId(),
                 customerDTO.getName(),
                 customerDTO.getAddress());
         customerRepo.save(customer);
+        return true;
+    }
+
+    public List<CustomerDTO> getAllCustomers() {
+        List<Customer> customerList=customerRepo.findAll();
+        List<CustomerDTO> customerDTOList=new ArrayList<>();
+        for (Customer customer : customerList) {
+            customerDTOList.add(new CustomerDTO(customer.getId(), customer.getName(), customer.getAddress()));
+        }
+        return customerDTOList;
+    }
+
+    public boolean updateCustomer(CustomerDTO customerDTO) {
+        if (customerRepo.existsById(customerDTO.getId())) {
+            Customer customer = new Customer(
+                    customerDTO.getId(),
+                    customerDTO.getName(),
+                    customerDTO.getAddress());
+            customerRepo.save(customer);
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean deleteCustomer(int id) {
+        customerRepo.deleteById(id);
         return true;
     }
 }
